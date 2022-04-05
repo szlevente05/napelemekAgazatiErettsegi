a27ab3d93d09bcdbf667625611a355896bd88b4d
Szürke Levente István <20d_szurkel@NYIRSZIKSZI.local>
2022-04-05 11:51:09 +0200

    First release of 'Napelemek'.

---
+++ napelem.js
@@ -0,0 +1,12 @@
+function panelek(db){
+    const panelek=document.getElementById('panelek');
+    panelek.innerHTML="";
+    for (i = 0; i < db; i++) {
+        panelek.innerHTML += "<img src='napelem_ikon.png'>"; 
+    }
+    panelek.innerHTML += "<br>"
+    panelek.innerHTML += "("+db+" db)";
+    document.getElementById('osszteljesitmeny').value = db * 275;
+    document.getElementById("tetofelulet").value = db * 3,5
+}
+panelek(8)
